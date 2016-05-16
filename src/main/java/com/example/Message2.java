ae7be81d6c082297cdb9de84c1e8f443a7775183
Daniel López <dlopezp@users.noreply.github.com>
2016-05-16 21:30:14 +0200

    Create Message2.java
    asdad

---
+++ src/main/java/com/example/Message2.java
@@ -0,0 +1,21 @@
+package com.example;
+
+public class Message2 {
+
+	private String value;
+	
+	public Message2() {}
+	
+	public Message2 (String value) {
+		this.value = value;
+	}
+	
+	public String getValue() {
+		return this.value;
+	}
+	
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+}
